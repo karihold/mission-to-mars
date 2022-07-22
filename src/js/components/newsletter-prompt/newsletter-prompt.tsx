8c4cea1a536b7eb9f47af026cd5a8dd7f911d903
karihold <holdcroftkari@gmail.com>
2022-07-22 15:06:01 +0200

    added newspaper component

---
+++ src/js/components/newsletter-prompt/newsletter-prompt.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from "react";
+import "./newsletter-prompt.scss";
+
+const NewsletterPrompt = () => {
+  return (
+    <a href="/newsletter" className="newsletter-frontpage-link">
+      <span>Join our </span>
+      <span>newsletter</span>
+    </a>
+  );
+};
+
+export default NewsletterPrompt;
